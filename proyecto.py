4115aa18d57ec74a05d4706aa24e108be0036885
Thiago Sandoval <thiagosandoval2514@gmail.com>
2026-06-10 19:03:46 +0000

    Los indices del grafico ahora muestran el nombre del combustible

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import csv
+import matplotlib.pyplot as plt
+
 #print ("Hola")
 #with open ('precios_surtidor_2024_2025_2026.csv', mode='r', encoding='utf-8') as file:
 #    lectura = csv.DictReader(file)
@@ -26,7 +28,11 @@ with open ('precios_surtidor_2024_2025_2026.csv', mode='r', encoding='utf-8') as
             cant_nafta_gasoilG2 +=1
         else:
             cant_nafta_gasoilG3 +=1
-st.button("Hola")       
-st.bar_chart({"data": [cant_nafta_super, cant_nafta_premium, cant_nafta_GNC, cant_nafta_gasoilG2, cant_nafta_gasoilG3]})
+#st.button("Hola")     #Boton de prueba
+#st.bar_chart({"data": [cant_nafta_super, cant_nafta_premium, cant_nafta_GNC, cant_nafta_gasoilG2, cant_nafta_gasoilG3]}) #Grafico de barras pero con indices 0,1,2,3,4
+
+combustible = {"Nafta Super": cant_nafta_super, "Nafta Premium": cant_nafta_premium, "GNC": cant_nafta_GNC, "Gasoil G2": cant_nafta_gasoilG2, "Gasoil G3": cant_nafta_gasoilG3}
+
+st.bar_chart(combustible)
 
 #python -m streamlit run proyecto.py (hostear proyecto)
