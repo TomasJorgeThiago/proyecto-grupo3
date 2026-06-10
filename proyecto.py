b9c1a13eb8e621f705c5bcf028c7c747138d731b
Tomas Tosoni <tomiitosonii@gmail.com>
2026-06-10 22:07:40 +0000

    Receta + main

--- proyecto.py
+++ proyecto.py
@@ -9,7 +9,17 @@ import matplotlib.pyplot as plt
 #      print(row['producto'], row['empresabandera'])
 
 
-with open ('precios_surtidor_2024_2025_2026.csv', mode='r', encoding='utf-8') as file:
+with open ('precios_surtidor_2024_2025_2026.csv', mode='r', encoding='utf-8') as file: 
+    #lectura= lo usamos para leer el csv, utilizamos el modulo: csv
+    #Cantidad de nafta:
+    #cant_nafta_"tipo de nafta": Lo usamos para contabilizar cada tipo de nafta que hay en argentina,
+    #utilizamos un contador, por iteracion va sumando 1 al tipo de nafta, por el 'idproducto' correspondiente.
+    #Nafta Super = idproducto 2
+    #Nafta Premium = idproducto 3
+    #Nafta GNC = idproducto 6
+    #Nafta Gasoil Grado 2 = idproducto 19
+    #Nafta Gasoil Grado 2 = idproducto 21
+
     lectura = csv.DictReader(file)
     cant_nafta_super= 0
     cant_nafta_premium= 0
@@ -31,8 +41,14 @@ with open ('precios_surtidor_2024_2025_2026.csv', mode='r', encoding='utf-8') as
 #st.button("Hola")     #Boton de prueba
 #st.bar_chart({"data": [cant_nafta_super, cant_nafta_premium, cant_nafta_GNC, cant_nafta_gasoilG2, cant_nafta_gasoilG3]}) #Grafico de barras pero con indices 0,1,2,3,4
 
-combustible = {"Nafta Super": cant_nafta_super, "Nafta Premium": cant_nafta_premium, "GNC": cant_nafta_GNC, "Gasoil G2": cant_nafta_gasoilG2, "Gasoil G3": cant_nafta_gasoilG3}
-
-st.bar_chart(combustible)
+combustible = {"Nafta Super": cant_nafta_super, 
+               "Nafta Premium": cant_nafta_premium, 
+               "GNC": cant_nafta_GNC, 
+               "Gasoil G2": cant_nafta_gasoilG2, 
+               "Gasoil G3": cant_nafta_gasoilG3}
+def main():
+    #Grafico que muestra, la cantidad de combustible por tipo
+    st.bar_chart(combustible)
+main()
 
 #python -m streamlit run proyecto.py (hostear proyecto)
